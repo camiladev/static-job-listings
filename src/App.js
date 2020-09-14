c475d5f30470cf7abfb2a3f5a1ddd640a87e2c06
Camila Matos <camila.mila148@gmail.com>
2020-09-14 10:57:07 -0300

    Ajustes no layout base

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import background from './images/bg-header-mobile.svg';
+import logo from './images/photosnap.svg';
 import './style/css/index.css';
 
 function App() {
@@ -10,6 +10,40 @@ function App() {
         
       </header>
       <body className="App-body">
+        <div className="filterTable">
+          <table>
+            <tbody>
+                <tr className="logo">
+                  <img src={logo} alt="Logo" width="55" height="55" />
+                </tr>
+                <tr className="company">
+                  <td className="name">Photosnap</td>
+                  <td className="status">
+                    <span className="new">New!</span>
+                    <span className="featured">Featured</span>
+                    
+                  </td>
+                  
+                </tr>
+                <tr className="position">Senior Frontend Developer</tr>
+                <tr className="infos">
+                  <td>1d ago</td>
+                  <div></div>
+                  <td>Full Time</td>
+                  <div></div>
+                  <td>USA Only</td>
+                </tr>
+                <tr className="line"><span></span></tr>
+                <tr className="filters">
+                  <td>Languages</td>
+                  <td>Tools</td>
+                  <td>Role</td>
+                  <td>Level</td>
+                </tr>
+            </tbody>
+          </table>
+
+        </div>
 
       </body>
     </div>
