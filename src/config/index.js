0421f8829249f6981c05f08e1c76c6b9b539fccb
Camila Matos <camila.mila148@gmail.com>
2020-10-01 09:58:53 -0300

    Incluido backend herokuapp

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = 
-    window.location.hostname.includes('localhost') &&
-    'http://localhost:8080' ;
+    window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080' : 'https://static-job-listings.herokuapp.com/' ;
 
 
 export default {
